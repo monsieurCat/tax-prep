8cfdc368dbbef7913f7a571c3172b788e7adda09
monsieurCat <falharbi.m.96@gmail.com>
2024-04-20 18:40:49 -0700

    start dockerization implementation

--- server/src/main/java/com/skillstorm/taxprep/server/ServerApplication.java
+++ server/src/main/java/com/skillstorm/taxprep/server/ServerApplication.java
@@ -2,9 +2,17 @@ package com.skillstorm.taxprep.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ServerApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
